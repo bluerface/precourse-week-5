da2b6384d1479d37b7c46e1285ba13cde1a0626e
Zoe Lawrence <fallingsilkworm@gmail.com>
2017-02-15 17:58:17 +0000

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,8 @@
 var questions = [
   {
     q: "This is some question text",
-    a: ["this is not the answer",
+    a: [
+        "this is not the answer",
         "this could be the answer, if you're not very bright",
         "this is maybe the answer wink",
         "this answer is an in joke"
@@ -9,21 +10,33 @@ var questions = [
     correct: 2
   },
   {
-    q: "question two",
-    a: ["akfddkja;lkjf",
-        "butt",
-        "djpoijvpajdf"
+    q: "What is your favourite fruit?",
+    a: [
+        "pinapple",
+        "chicken",
+        "kumkwat"
       ],
-    correct: 1
+    correct: 2
   },
   {
-    q: "question three",
-    a: ["jakdfjahdfkjh",
-        "vyouhadfj",
-        "adhvouhijadnf",
-        "hello"
+    q: "What is your mother's maiden name?",
+    a: [
+        "Axolotl",
+        "Bobson",
+        "Smith",
+        "Wingdings"
       ],
-    correct: 3
-  }
+    correct: 0
+  },
+  {
+    q: "Will you give me a job?",
+    a: [
+      "Yes",
+      "When can you start?",
+      "Maybe if you bribe us with cookies",
+      "Items in mirror may be larger than they seem"
+      ],
+    correct: 2
+  } 
 
 ]
